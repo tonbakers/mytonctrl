250485c15423343548163c0491cda8dcd2376395
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-09-16 14:37:50 +0300

    [TS-2] Improve installer of ton-http-api

--- scripts/ton_http_api_installer.sh
+++ scripts/ton_http_api_installer.sh
@@ -25,7 +25,28 @@ python3 setup.py install
 
 
 echo -e "${COLOR}[2/3]${ENDC} Add to startup"
-cmd="from sys import path; path.append('/usr/src/mytonctrl/');  from mypylib.mypylib import *; Add2Systemd(name='ton-http-api', user='${user}', workdir='/usr/src/ton-http-api', start='/usr/bin/python3 -m ton_http_api --liteserverconfig /usr/bin/ton/local.config.json --libtonlibjson /usr/bin/ton/tonlib/libtonlibjson.so')"
+service_name=""
+extend_path="from sys import path; path.append('/usr/src/ton-http-api/');"
+import_mypylib="from mypylib.mypylib import *;"
+
+pyton_executable_path="/usr/bin/python3 ton-http-api/pyTON"
+liteserver_config_path="/usr/bin/ton/global.config.json"
+libtonlibjson_path="/usr/bin/ton/tonlib/libtonlibjson.so"
+mkdir ~/keystore
+touch ~/keystore/keystore.ks
+ton_config="--liteserver-config ${liteserver_config_path} --cdll-path ${libtonlibjson_path} --parallel-requests-per-liteserver 100 --tonlib-keystore ~/keystore/keystore.ks"
+server_config="--host $(hostname -I) --port 8073"
+
+arg_name="name='ton-http-api'"
+arg_user="user='${user}'"
+arg_workdir="workdir='/usr/src/ton-http-api'"
+arg_start="start='${pyton_executable_path} ${ton_config}'"
+
+systemd_arguments="${arg_name}, ${arg_user}, ${arg_workdir}, ${arg_start}"
+systemd_call="Add2Systemd(${systemd_arguments})"
+
+cmd="${extend_path}${import_mypylib}"
+mkdir /usr/bin/ton/keystore/ton-keystore
 python3 -c "${cmd}"
 systemctl restart ton-http-api
 
